3a307ae53c016955aed75d2d5bd5fb27fb45db31
zivenlopez <zivenlopez@gmail.com>
2023-12-09 22:37:19 -0500

    Square is drawn on screen (will become snake)

--- Snake.py
+++ Snake.py
@@ -1,24 +1,26 @@
 """
-This creates a basic 500x400 window
-also tracks position of mouse and click state
+Single square is drawn on screen
+will be made into snake
 """
 
 import pygame
 pygame.init()
+dis = pygame.display.set_mode((600,500))
 
-dis = pygame.display.set_mode((500,400))
-
-pygame.display.update()
 pygame.display.set_caption('Snake game by Ziven')
+
+blue = (0, 0, 255)
+red = (255, 0, 0)
 
 game_over = False
 
 while not game_over:
     for event in pygame.event.get():
-        print(event)     #prints out all the actions that take place on screen
-
+        
         if event.type == pygame.QUIT:
-            game_over=True
+            game_over = True
 
+    pygame.draw.rect(dis, blue, [200,150,10,10])
+    pygame.display.update()
 pygame.quit()
 quit()

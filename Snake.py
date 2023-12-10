e459a13379083254d01c42651d0222e1cee684f4
zivenlopez <zivenlopez@gmail.com>
2023-12-09 22:39:04 -0500

    Green square movement, corresponding to arrow keys

--- Snake.py
+++ Snake.py
@@ -1,26 +1,60 @@
 """
-Single square is drawn on screen
-will be made into snake
+Green square movement, corresponding to arrow keys
 """
 
 import pygame
 pygame.init()
-dis = pygame.display.set_mode((600,500))
 
-pygame.display.set_caption('Snake game by Ziven')
-
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0 , 0)
+green = (0, 255, 0)
 blue = (0, 0, 255)
-red = (255, 0, 0)
+
+dis = pygame.display.set_mode((1000,800))
+pygame.display.set_caption('Snake game by Ziven')
 
 game_over = False
 
+x1 = 300
+y1 = 300
+
+x1_change = 0
+y1_change = 0
+
+clock = pygame.time.Clock()
+
 while not game_over:
     for event in pygame.event.get():
-        
+        # if the user clicks QUIT button
         if event.type == pygame.QUIT:
             game_over = True
+        
+        # if a KEY is pressed
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:   # left button pressed
+                x1_change = -10
+                y1_change = 0
+            elif event.key == pygame.K_RIGHT:   # right button pressed
+                x1_change = 10
+                y1_change = 0
+            elif event.key == pygame.K_UP:   # up button pressed
+                x1_change = 0
+                y1_change = -10
+            elif event.key == pygame.K_DOWN:   # down button pressed
+                x1_change = 0
+                y1_change = 10
+    
+    # movement control
+    x1 += x1_change
+    y1 += y1_change
+    
+    dis.fill(black)
 
-    pygame.draw.rect(dis, blue, [200,150,10,10])
+    pygame.draw.rect(dis, green, [x1, y1, 20, 20])
     pygame.display.update()
+
+    clock.tick(20)
+
 pygame.quit()
 quit()
